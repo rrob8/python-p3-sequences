3f638852832e63f8d9f0103e9e7527e681e275fb
Robert Bubnik <rbubnik@gmail.com>
2023-12-21 19:54:08 -0500

    passing test

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    i = 1
+    my_list = []
+    if length > 0:
+        my_list.append(0)
+        if length > 1:
+            my_list.append(1)
+            if length > 2:
+                while len(my_list) < length:
+                    my_list.append(my_list[i]+my_list[i-1])
+                    i +=1
+                print(my_list)
+                return
+    print(my_list)
+    return
